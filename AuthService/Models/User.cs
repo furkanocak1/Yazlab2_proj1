cfd88f9dde89a3b7530305742e879cb0714965a4
Cüneyt Şendur <144320109+cuneytsendurr@users.noreply.github.com>
2026-03-24 18:29:58 +0300

    s

--- AuthService/Models/User.cs
+++ AuthService/Models/User.cs
@@ -7,7 +7,7 @@ namespace AuthService.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         public string StudentId { get; set; } // Öðrenci Numarasý
 
